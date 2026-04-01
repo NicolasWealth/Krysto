a7a02fbe34f2b0865331ac4a74f9a00346d21701
NicolasWealth <152969431+NicolasWealth@users.noreply.github.com>
2026-04-01 13:42:38 +0100

    Add files via upload

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,3 @@
-console.log(import.meta.env)
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
@@ -8,7 +7,7 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
