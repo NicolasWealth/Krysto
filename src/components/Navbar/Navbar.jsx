fbb4fde34ab0940f2bda4382d35c0806054a9804
NicolasWealth <152969431+NicolasWealth@users.noreply.github.com>
2026-04-01 22:03:46 +0100

    Add files via upload

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,1 +1,15 @@
+import "./Navbar.css"
 
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <a href="#" className="navbar-brand">
+          Krysto<span>.</span>
+        </a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
